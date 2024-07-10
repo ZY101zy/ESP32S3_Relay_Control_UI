31f6d74eb0fee60a6b21de6fdc01d326b7e9e7f2
ZY101zy <zhouyi@espressif.com>
2024-07-10 15:48:12 +0800

    workspace setting

--- main/main.c
+++ main/main.c
@@ -1,5 +1,13 @@
 /*
- * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ * @Author: ZY101zy zhouyi@espressif.com
+ * @Date: 2024-07-10 13:36:22
+ * @LastEditors: ZY101zy zhouyi@espressif.com
+ * @LastEditTime: 2024-07-10 15:39:39
+ * @FilePath: /relay_control/main/main.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
+ * SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: CC0-1.0
  */
@@ -10,6 +18,7 @@
 /*******************************************************************************
 * Private functions
 *******************************************************************************/
+
 
 void relay_control_init()
 {
@@ -26,7 +35,7 @@ void relay_control_init()
     //enable pull-up mode
     io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
     //configure GPIO with the given settings
-    gpio_config  (&io_conf);
+    gpio_config(&io_conf);
     gpio_set_level(RELAY_CTRL_PIN, RELAY_OFF);
 
 }
